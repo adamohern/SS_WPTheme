4f3e010cceff961fb915fbf27d7f3c3e011a8a24
joshmings <goatlip@gmail.com>
2015-05-08 10:35:04 -0500

    Code clean up

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="footer">
 	<div class="inner group">
 		<div class="col foot-title">
-			<h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ss-logo.png" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a></h2>
+			<h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ss-logo.png" width="245px" height="42px" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a></h2>
 			<?php get_search_form(); ?>
 		</div>
 		<div class="col foot-social">
