15ef74d49b9502f1bfe0ac533c53ffa2182c16a7
adamohern <adam@evd1.tv>
2016-08-24 14:55:54 -0400

    more

--- index.php
+++ index.php
@@ -37,28 +37,12 @@
             if ($query->have_posts()) {
               $i = 0;
               while ($query->have_posts()) {
-                if ($i == 2) { ?>
-                  <article class="small_post" style="position: absolute; left: 767px; top: 0px;">
-                    <div class="top">
-                        <div class="cats_def">
-                            <a href="http://solidsmack.wpstagecoach.com/category/radio/" class="cat-4738">SPONSORED</a>
-                          </div>
-                        </div>
-                    <h2><a href="http://solidsmack.wpstagecoach.com/radio/solidsmack-radio-fillet-funkdown-lost-my-edge-2/">Brought to you by Ketchup</a></h2>
-                    <div class="post-meta">
-                      <span class="admin_meta"><a class="admin" href="http://solidsmack.wpstagecoach.com/author/solidsmack/">Ketchup</a></span>
-                	 </div>
-
-                    <figure>
-                         <div class="social_icon  social_icon_inimage small_social_inimage"><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fsolidsmack.wpstagecoach.com%2Fradio%2Fsolidsmack-radio-fillet-funkdown-lost-my-edge-2%2F" class="fb share-link-image" data-service="facebook"><i class="fa fa-facebook"></i></a><a href="https://twitter.com/share?url=http%3A%2F%2Fsolidsmack.wpstagecoach.com%2Fradio%2Fsolidsmack-radio-fillet-funkdown-lost-my-edge-2%2F&amp;text=SolidSmack+Radio+%7C+VR+Technicolor+Daydream" class="tw share-link-image" data-service="twitter"><i class="fa fa-twitter"></i></a><a href="https://pinterest.com/pin/create/button/?url=http%3A%2F%2Fsolidsmack.wpstagecoach.com%2Fradio%2Fsolidsmack-radio-fillet-funkdown-lost-my-edge-2%2F&amp;media=http://solidsmack.wpstagecoach.com/wp-content/uploads/2016/02/FEATURE.jpg&amp;description=SolidSmack+Radio+%7C+VR+Technicolor+Daydream" class="pn share-link-image" data-service="pinterest"><i class="fa fa-pinterest-p"></i></a></div>                         <div class="pattern"></div>
-                         <a href="http://solidsmack.wpstagecoach.com/radio/solidsmack-radio-fillet-funkdown-lost-my-edge-2/"><img src="http://solidsmack.wpstagecoach.com/wp-content/uploads/2016/02/FEATURE.jpg" width="336" height="220" alt="SolidSmack Radio | VR Technicolor Daydream"></a>
-                    </figure>
-
-                    <p>Do you like ketchup? So do we. Here's why.</p>
-                  	<a href="http://solidsmack.wpstagecoach.com/radio/solidsmack-radio-fillet-funkdown-lost-my-edge-2/" class="btn_more">READ MORE  +</a>
-
+                if ($i == 2) {
+                  ?>
+                  <article class="small_post sponsored">
+                  <div class="gridBanner">banner</div>
                   </article>
-                <?php
+                  <?php
                 } else {
                   $query->the_post();
                   if (rehub_option('rehub_framework_archive_layout') == 'rehub_framework_archive_blog') :
